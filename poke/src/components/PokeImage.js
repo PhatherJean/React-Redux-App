bd054ef8fc6464e7b7e8c20c382d8bfeeb3c24b1
Patrice Jean <73203091+PhatherJean@users.noreply.github.com>
2021-02-17 22:13:07 -0500

    update app

---
+++ poke/src/components/PokeImage.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const PokeImage = (props) => {
+  const [image, setImage] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`${props.poke.url}`)
+      .then((res) => setImage(res.data.sprites))
+      .catch((err) => console.log("img", err));
+  }, []);
+
+  return (
+    <div className="pokewrap">
+      <h3>{props.poke.name}</h3>
+      <img src={image.front_default} alt={props.poke.name} />
+    </div>
+  );
+};
+export default PokeImage;
